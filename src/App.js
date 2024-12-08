21ffc78c9f0130f9d1f5ba5092537ea5b5217aa5
Mehedi hasan <162013416+Meehedi-hasan@users.noreply.github.com>
2024-12-09 01:32:59 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import Home from './Components/Home/Home'
 import Footer from "./Components/Share/Footer";
 import Header from "./Components/Share/Header";
